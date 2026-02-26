43cae25b6b804181401a3e44aa13118ac8ad4558
Ouedraogo Grace Olivier <ouedraogograceolivier5@gmail.com>
2026-02-26 19:53:25 +0000

    Update DATABASES configuration for SSL support
    Pour plus de diversité

--- djangoApp/monSiteWeb/monSiteWeb/settings.py
+++ djangoApp/monSiteWeb/monSiteWeb/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'monSiteWeb.wsgi.application'
 }"""
 
 DATABASES = {
-    #'default': dj_database_url.config(default='postgres://...')
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
